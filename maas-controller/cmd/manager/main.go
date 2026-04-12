94df892a7fd860d8b8cc7459eb88110176c03764
Ishita Sequeira <ishiseq29@gmail.com>
2026-04-12 13:26:53 -0400

    feat(maas-controller): add MaaSTenant API, CRD, and stub reconciler

--- maas-controller/cmd/manager/main.go
+++ maas-controller/cmd/manager/main.go
@@ -447,6 +447,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&maas.MaaSTenantReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MaaSTenant")
+		os.Exit(1)
+	}
+
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
